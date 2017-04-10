08ade749b9b00bcbcaaadc8474d9556a13842791
Leon <yanglyu.leon.7@gmail.com>
2017-04-10 11:04:31 -0700

    StackAppWidget has been improved and thus worked. Custom object/class cannot be passed via FillInIntent when StackWidgetService being setup, but a position int can do.

--- app/src/main/java/com/leontheprofessional/test/whorepresentsyou/appwidget/StackWidgetProvider.java
+++ app/src/main/java/com/leontheprofessional/test/whorepresentsyou/appwidget/StackWidgetProvider.java
@@ -7,25 +7,44 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import com.leontheprofessional.test.whorepresentsyou.R;
 import com.leontheprofessional.test.whorepresentsyou.MainActivity;
 import com.leontheprofessional.test.whorepresentsyou.activity.MemberDetailsActivity;
 import com.leontheprofessional.test.whorepresentsyou.appwidget.service.StackWidgetService;
+import com.leontheprofessional.test.whorepresentsyou.helper.GeneralHelper;
 import com.leontheprofessional.test.whorepresentsyou.model.MemberModel;
+
+import java.lang.reflect.Member;
+import java.util.ArrayList;
 
 /**
  * Created by Leon on 10/19/2015.
  */
 public class StackWidgetProvider extends AppWidgetProvider {
 
+    private static final String LOG_TAG = StackWidgetProvider.class.getSimpleName();
+
     private static final String DISPLAY_ACTION = "com.leontheprofessional.test.whorepresentsyou.DISPLAY_ACTION";
 
+    private ArrayList<MemberModel> membersArrayList;
+    private MemberModel member;
+
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(final Context context, Intent intent) {
         if (intent.getAction() == DISPLAY_ACTION) {
-            MemberModel member = intent.getExtras().getParcelable(StackWidgetService.INTENT_EXTRAS_MEMBER_IDENTIFIER);
+
+            int position = intent.getExtras().getInt(StackWidgetService.INTENT_EXTRA_POSITION_IDENTIFIER);
+            Log.i(LOG_TAG, "position: " + position);
+
+            membersArrayList = GeneralHelper.getAllFavoriteMembers(context);
+
+            member = membersArrayList.get(position);
+            Log.i(LOG_TAG, "member name: " + member.getName());
             Intent displayIntent = new Intent(context, MemberDetailsActivity.class);
             Bundle bundle = new Bundle();
             bundle.putParcelable(context.getString(R.string.parcelable_identifier), member);
@@ -33,12 +52,18 @@ public class StackWidgetProvider extends AppWidgetProvider {
             displayIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             displayIntent.setAction(MainActivity.CUSTOM_SEARCH_INTENT_FILTER);
             context.startActivity(displayIntent);
+
         }
         super.onReceive(context, intent);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        for (int i = 0; i < appWidgetIds.length; i++) {
+            Log.i(LOG_TAG, "Contents of appWidgetIds: " + appWidgetIds[i]);
+        }
+
         for (int i = 0; i < appWidgetIds.length; i++) {
 
             // set intent for widget service that will create the views
@@ -54,6 +79,7 @@ public class StackWidgetProvider extends AppWidgetProvider {
             Intent viewIntent = new Intent(context, StackWidgetProvider.class);
             viewIntent.setAction(StackWidgetProvider.DISPLAY_ACTION);
             viewIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+            Log.i(LOG_TAG, "appWidgetIds[i]: " + appWidgetIds[i]);
             viewIntent.setData(Uri.parse(viewIntent.toUri(Intent.URI_INTENT_SCHEME)));
 
             PendingIntent viewPendingIntent = PendingIntent.getBroadcast(context, 0, viewIntent, PendingIntent.FLAG_UPDATE_CURRENT);
@@ -63,5 +89,11 @@ public class StackWidgetProvider extends AppWidgetProvider {
             appWidgetManager.updateAppWidget(appWidgetIds[i], remoteViews);
         }
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+        Toast.makeText(context, context.getString(R.string.widget_deleted_confirmation), Toast.LENGTH_SHORT).show();
     }
 }
